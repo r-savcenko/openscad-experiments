268490e6980c1538e5c45566dab7dbfa314592d4
Ruslan Savcenko <ruslan.savcenko@gmail.com>
2020-03-11 06:27:03 +0200

    another 1

---
+++ bottle_case/generate.bat
@@ -0,0 +1,6 @@
+openscad -o .\panel-1.stl .\base.scad
+openscad -o .\panel-2.stl .\base.scad
+openscad -o .\panel-3.stl .\base.scad
+openscad -o .\panel-4.stl .\base.scad
+openscad -o .\panel-5.stl .\base.scad
+openscad -o .\panel-6.stl .\base.scad
